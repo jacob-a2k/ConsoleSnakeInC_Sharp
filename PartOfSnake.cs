7d54fae4ac4a0f13851bf9ed12244007167947f7
jacob-a2k <jakub.a2k@gmail.com>
2023-09-25 21:12:43 +0200

    create PartOfSnake class

---
+++ PartOfSnake.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSnakeGame
+{
+    public class PartOfSnake
+    {
+        private Point position;
+        private PartOfSnake nextPart;
+        public PartOfSnake(Point position)
+        {
+            this.position = position;
+            this.nextPart = nextPart;
+        }
+        public PartOfSnake(Point position, PartOfSnake nextPart)
+        {
+            this.position = position;
+            this.nextPart = nextPart;
+        }
+        public Point GetPosition()
+        {
+            return this.position;
+        }
+        public void SetPosition(Point position)
+        {
+            this.position = position;
+        }
+        public PartOfSnake GetNext()
+        {
+            return nextPart;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,6 @@ namespace ConsoleSnakeGame
         public static void Main(string[] args)
         {
             Map map = new Map();
-            //map.PrintMap();
-
-            Point point = new Point(6, 8);
-            map.SetToMap(point, 'x');
             map.PrintMap();
         }
 
